df95508bc51cb66910d861aeb12c94837cf08840
Nat Eliason <nateliason@gmail.com>
2016-11-18 09:10:27 -0300

    almost there

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,22 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function moveText(amount){
+  target = document.querySelector("#articleDisplay");
+  targetPosition = target.getAttribute("position");
+  stepCount = 0
+  var id = setInterval(frame, 5);
+  function frame(){
+    if (stepCount == 100) {
+        clearInterval(id);
+    } else {
+        stepCount++; 
+        target.setAttribute('position',{
+          x: targetPosition.x,
+          y: targetPosition.y + (amount/100 * stepCount),
+          z: targetPosition.z
+        });
+    };
+  };
+}
